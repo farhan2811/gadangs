1bddfe95be36ec432edbba1fc1068331f096a947
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2023-05-27 00:02:37 +0700

    creating slug folder so sveltekit could get the params

--- src/routes/super-admin/nup/detail-nup/[slug]/+page.svelte
+++ src/routes/super-admin/nup/detail-nup/[slug]/+page.svelte
@@ -1,9 +1,29 @@
 <script type="text/javascript">
-	import Sidebar from '../../../../components/sidebar.svelte'
-	import Navbar from '../../../../components/navbar.svelte'
+	import Sidebar from '../../../../../components/sidebar.svelte'
+	import Navbar from '../../../../../components/navbar.svelte'
 	import { onMount } from 'svelte';
 	import {fly, scale} from 'svelte/transition'
+	import { page } from '$app/stores'
+	import ApiController from '../../../../../ApiController';
+
+	export let data
 	
+	let idNup = data.params.slug
+	let nup = null
+
+	let getDetailNUP = () => {
+		ApiController({
+			method: "GET",
+			endpoint: `nup/${idNup}`
+		}).then(response => {
+			nup = response.data.data
+		})
+	}
+
+	onMount(() => {
+		getDetailNUP()
+	})
+
 </script>
 
 <div id="after-login-layout">
@@ -16,7 +36,9 @@
 					<div class="flex  flex-direction-col flex-between-horizontal flex-gap-semi-small w-100">
 						<div class="title-content">Detail NUP</div>
 						<div class="flex flex-gap-regular flex-center-vertical">
-							<div class="text-breadcrumb">NUP</div>
+							<div class="text-breadcrumb" on:click={() => {
+								history.back()
+							}}>NUP</div>
 							<img src="/images/icons/Arrow_Right.svg">
 							<div class="text-breadcrumb-active">Detail NUP</div>
 						</div>
@@ -26,6 +48,8 @@
 						<a href="/super-admin/nup/detail-nup" class="no-decor"><button class="btn-fill-danger flex flex-center-vertical flex-gap-small"><img src="/images/icons/Trash_White.svg"> <span>Hapus</span></button></a>
 					</div>
 				</div>
+
+				{#if nup != null}
 				<div class="card w-100 flex flex-gap-semi-large">
 					<div class="w-40">
 						<img class="img-detail w-100 h-100" src="/images/rumah.png">
@@ -33,8 +57,8 @@
 					<div class="w-60 flex flex-direction-col flex-gap-large">
 						<div class="flex flex-gap-regular">
 							<div class="w-50 flex flex-direction-col flex-gap-small">
-								<div class="main-title-card-detail">Gria Esma Cicalengka</div>
-								<div class="main-sub-title-card-detail">Nomor Urut Pemesan: 70081</div>
+								<div class="main-title-card-detail">{nup.perumahan}</div>
+								<div class="main-sub-title-card-detail">Nomor Urut Pemesan: {nup.id_nup}</div>
 							</div>
 							<div class="w-50 flex flex-gap-regular">
 								<div class="label-success flex flex-center-horizontal flex-gap-small">
@@ -52,20 +76,20 @@
 								<div class="secondary-title-card-detail">Pengajuan Unit</div>
 								<div class="flex flex-direction-col">
 									<div class="caption-card-detail">Tipe Pengajuan (KPR/KPA)</div>
-									<div class="content-card-detail">Subsidi</div>
+									<div class="content-card-detail">{nup.tipe_pengajuan}</div>
 								</div>
 								<div class="flex flex-direction-col">
 									<div class="caption-card-detail">Tanggal Submit</div>
-									<div class="content-card-detail">25 April 2018</div>
+									<div class="content-card-detail">{nup.pilihan_unit.tanggal_submit}</div>
 								</div>
 								<div class="flex w-100">
 									<div class="flex flex-direction-col w-50">
 										<div class="caption-card-detail">Harga Unit</div>
-										<div class="content-card-detail">Rp. 500.000.000</div>
+										<div class="content-card-detail">Rp. {nup.pilihan_unit.harga_unit}</div>
 									</div>
 									<div class="flex flex-direction-col">
 										<div class="caption-card-detail">Versi Price List</div>
-										<div class="content-card-detail">V.2018.04.14</div>
+										<div class="content-card-detail">{nup.versi_pricelist}</div>
 									</div>
 								</div>
 								<div class="flex flex-direction-col flex-gap-semi-small">
@@ -74,26 +98,26 @@
 										<div class="content-card-detail">Alternatif 1</div>
 										<div class="flex flex-direction-col">
 											<div class="caption-card-detail">Jenis Bangunan</div>
-											<div class="content-card-detail">Perumahan</div>
+											<div class="content-card-detail">{nup.pilihan_unit.jenis_bangunan}</div>
 										</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Blok</div>
-												<div class="content-card-detail">A2</div>
+												<div class="content-card-detail">{nup.pilihan_unit.tower_blok}</div>
 											</div>
 											<div class="flex flex-direction-col">
 												<div class="caption-card-detail">Nomor</div>
-												<div class="content-card-detail">02</div>
+												<div class="content-card-detail">{nup.pilihan_unit.nomor}</div>
 											</div>
 										</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Lantai</div>
-												<div class="content-card-detail">1 Lantai</div>
+												<div class="content-card-detail">{nup.pilihan_unit.lantai} Lantai</div>
 											</div>
 											<div class="flex flex-direction-col">
 												<div class="caption-card-detail">Tipe</div>
-												<div class="content-card-detail">35/66 m²</div>
+												<div class="content-card-detail">{nup.pilihan_unit.tipe_perumahan}</div>
 											</div>
 										</div>
 									</div>
@@ -103,16 +127,16 @@
 								<div class="secondary-title-card-detail">Marketing</div>
 								<div class="flex flex-direction-col">
 									<div class="caption-card-detail">Lead Marketing</div>
-									<div class="content-card-detail">Zaenal Muhtarom</div>
+									<div class="content-card-detail">{nup.lead_marketing}</div>
 								</div>
 								<div class="flex flex-direction-col">
 									<div class="caption-card-detail">Marketing</div>
-									<div class="content-card-detail">Zaenal Muhtarom</div>
+									<div class="content-card-detail">{nup.marketing}</div>
 								</div>
 								<div class="card-secondary flex flex-direction-col flex-gap-semi-small">
 									<div class="caption-card-detail">Dokumen Pendukung</div>
 									<div class="flex flex-direction-col">
-										<div class="caption-card-detail">Berkas NUP</div>	
+										<div class="caption-card-detail">Berkas NUP</div>
 										<a href="#" class="no-decor link-detail">lihat</a>
 									</div>
 								</div>
@@ -128,22 +152,22 @@
 									<div class="secondary-title-card-detail">Pemesan</div>
 									<div class="flex w-100">
 										<div class="flex flex-direction-col w-50">
-											<div class="caption-card-detail">Nama Lengkapi</div>
-											<div class="content-card-detail">Suryadi</div>
+											<div class="caption-card-detail">Nama Lengkap</div>
+											<div class="content-card-detail">{nup.client.nama_client}</div>
 										</div>
 										<div class="flex flex-direction-col w-50">
 											<div class="caption-card-detail">Nomor KTP</div>
-											<div class="content-card-detail">1234XXXXXXXXXXXX</div>
+											<div class="content-card-detail">{nup.client.no_ktp}</div>
 										</div>
 									</div>
 									<div class="flex w-100">
 										<div class="flex flex-direction-col w-50">
 											<div class="caption-card-detail">Jenis Kelamin</div>
-											<div class="content-card-detail">Laki-Laki</div>
+											<div class="content-card-detail">{nup.client.jenis_kelamin}</div>
 										</div>
 										<div class="flex flex-direction-col w-50">
 											<div class="caption-card-detail">Nomor Telepon</div>
-											<div class="content-card-detail">1234XXXXXXXXXXXX</div>
+											<div class="content-card-detail">{nup.client.no_hp}</div>
 										</div>
 									</div>
 									<div class="flex w-100">
@@ -153,27 +177,27 @@
 										</div>
 										<div class="flex flex-direction-col w-50">
 											<div class="caption-card-detail">Alamat Email</div>
-											<div class="content-card-detail">suryadi@gmail.com</div>
+											<div class="content-card-detail">{nup.client.email}</div>
 										</div>
 									</div>
 									<div class="flex w-100">
 										<div class="flex flex-direction-col w-40">
 											<div class="caption-card-detail">Alamat Lengkap</div>
-											<div class="content-card-detail">Jalan Sudirman No. 32A, RT. 003, RW.012, Kel. Dago, Kec. Coblong, Kota Bandung, Jawa Barat</div>
+											<div class="content-card-detail">{nup.client.alamat}</div>
 										</div>
 										<div class="w-10"></div>
 										<div class="flex flex-direction-col w-50 flex-gap-regular">
 											<div class="flex flex-direction-col">
 												<div class="caption-card-detail">Nomor NPWP</div>
-												<div class="content-card-detail">XX.XXX.XXX.X-XXX.XXX</div>
+												<div class="content-card-detail">{nup.client.npwp}</div>
 											</div>
 											<div class="flex flex-direction-col">
 												<div class="caption-card-detail">Pekerjaan</div>
-												<div class="content-card-detail">Karyawan Swasta</div>
+												<div class="content-card-detail">{nup.client.pekerjaan}</div>
 											</div>
 											<div class="flex flex-direction-col">
 												<div class="caption-card-detail">Koordinat Lokasi Usaha</div>
-												<div class="content-card-detail">Jawa Barat</div>
+												<div class="content-card-detail">{nup.client.koordinat_usaha}</div>
 											</div>
 										</div>
 									</div>
@@ -220,12 +244,12 @@
 										<div class="secondary-title-card-detail">Pasangan</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
-												<div class="caption-card-detail">Nama Lengkapi</div>
-												<div class="content-card-detail">Poniyah</div>
+												<div class="caption-card-detail">Nama Lengkap</div>
+												<div class="content-card-detail">{nup.client.nama_pasangan}</div>
 											</div>
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Nomor KTP</div>
-												<div class="content-card-detail">1234XXXXXXXXXXXX</div>
+												<div class="content-card-detail">{nup.client.no_ktp_pasangan}</div>
 											</div>
 										</div>
 										<div class="flex w-100">
@@ -267,27 +291,27 @@
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Status BI Checking</div>
-												<div class="content-card-detail">Ya</div>
+												<div class="content-card-detail">{nup.bi_checking.status}</div>
 											</div>
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Hasil BI Checking</div>
-												<div class="content-card-detail">Kolektibilitas 1</div>
+												<div class="content-card-detail">{nup.bi_checking.hasil_}</div>
 											</div>
 										</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Tanggal Pengajuan</div>
-												<div class="content-card-detail">27 April 2018</div>
+												<div class="content-card-detail">{nup.bi_checking.tanggal_pengajuan.split(" ")[0]}</div>
 											</div>
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Hasil BI Checking Pasangan</div>
-												<div class="content-card-detail">Kolektibilitas 1</div>
+												<div class="content-card-detail">{nup.bi_checking.hasil_pasangan}</div>
 											</div>
 										</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Tanggal Hasil BI Checking</div>
-												<div class="content-card-detail">4 Mei 2018</div>
+												<div class="content-card-detail">{nup.bi_checking.tanggal_hasil.split(" ")[0]}</div>
 											</div>
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Nama Referensi</div>
@@ -310,27 +334,27 @@
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Nominal Booking Fee</div>
-												<div class="content-card-detail">Rp. 1.000.000</div>
+												<div class="content-card-detail">Rp. {nup.pembayaran.booking_fee}</div>
 											</div>
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Metode Pembayaran</div>
-												<div class="content-card-detail">Transfer</div>
+												<div class="content-card-detail">{nup.pembayaran.metode_pembayaran}</div>
 											</div>
 										</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Nama Bank</div>
-												<div class="content-card-detail">Bank Mandiri</div>
+												<div class="content-card-detail">{nup.pembayaran.bank}</div>
 											</div>
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Nomor Rekening</div>
-												<div class="content-card-detail">XXXXXXXXXXXX</div>
+												<div class="content-card-detail">{nup.pembayaran.no_rek}</div>
 											</div>
 										</div>
 										<div class="flex w-100">
 											<div class="flex flex-direction-col w-50">
 												<div class="caption-card-detail">Tanggal Pembayaran</div>
-												<div class="content-card-detail">19 April 2018</div>
+												<div class="content-card-detail">{nup.pembayaran.tanggal_pembayaran}</div>
 											</div>
 										</div>
 									</div>
@@ -354,6 +378,7 @@
 					<button class="btn-transparent flex flex-center-vertical flex-gap-small"><span>Lihat Detail Data Unit</span></button>
 					<button class="btn-transparent flex flex-center-vertical flex-gap-small"><span>Lihat Detail Data Pemesan</span></button>
 				</div>
+				{/if}
 			</div>
 		</div>
 	</div>
